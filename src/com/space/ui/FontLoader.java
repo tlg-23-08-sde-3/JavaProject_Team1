fd40830ce86546f8e2874dd5b1c580095b7c68ac
jtnoble <jtnoble321@gmail.com>
2023-09-20 11:53:30 -0500

    UI Refactoring and created FontLoader

---
+++ src/com/space/ui/FontLoader.java
@@ -0,0 +1,26 @@
+package com.space.ui;
+
+import java.awt.*;
+import java.io.File;
+
+public class FontLoader {
+
+    private FontLoader() {
+        // no-op, static class
+    }
+
+    public static Font loadFont(String fontString, float fontSize) {
+        Font font;
+        try {
+            File file = new File(fontString);
+            font = Font.createFont(Font.TRUETYPE_FONT, file);
+            font = font.deriveFont(fontSize);
+        }
+        catch (Exception e) {
+            System.out.println(e.getLocalizedMessage());
+            font = new Font("Arial", Font.PLAIN, 22);
+        }
+        return font;
+
+    }
+}
